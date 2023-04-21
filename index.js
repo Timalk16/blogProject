a19ccad2f7ea63ab4efa99034e82be1bb6560d60
Timalk16 <tima160604@mail.ru>
2023-04-21 16:35:43 +0300

    add checkuth

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ import mongoose from 'mongoose';
 
 import { validationResult } from "express-validator";
 import { registerValidation } from './validations/auth.js'
+
 import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+import User from "./models/User.js";
 
 mongoose
     .connect('mongodb+srv://admin:wwwwww@cluster0.jclvzgg.mongodb.net/blog?retryWrites=true&w=majority')
@@ -111,7 +114,25 @@ app.post('/auth/register', registerValidation, async (req, res) => {
     }
 });
 
-app.get('/auth/')
+app.get('/auth/me', checkAuth,  async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if (!user) {
+            return  res.status(404).json({
+                message: 'Пользователь не найден'
+            });
+        }
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(userData);
+    } catch (e) {
+        return  res.status(500).json({
+            message: 'Нет доступа'
+        });
+    }
+});
 
 app.listen(4444, (err) => {
     if (err) {
