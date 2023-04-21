5680b397b5f071417059650aa229f1a205af4c2b
Timalk16 <tima160604@mail.ru>
2023-04-21 17:08:15 +0300

    add validation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation } from './validations/auth.js'
+import { registerValidation, loginValidation } from './validations.js'
 import checkAuth from './utils/checkAuth.js'
 import * as UserController from './controllers/UserController.js'
 
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 
 // авторизация
-app.post('/auth/login', UserController.login);
+app.post('/auth/login', loginValidation, UserController.login);
 // регистрация с валидацией
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe());
